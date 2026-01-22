05df36bb0892bde144834e91c8337eef86d6dd9c
Muhammad Ali <mohammadalitipu5@gmail.com>
2026-01-22 17:20:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const _ = require("lodash");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
+const path = require("path");
+app.set("views", path.join(__dirname, "views"));
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -118,6 +123,6 @@ app.get("/:customListName", async (req, res) => {
   }
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000.");
+app.listen(port, () => {
+  console.log("Server started on port " + port);
 });
